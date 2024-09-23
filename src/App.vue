7ebe408b241d70f24520859c5640667538acdbd9
gervais <gervaisamoah@yahoo.com>
2024-09-23 23:58:50 +0000

    Starting the HTML

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
 <template>
-  <main>
-    ok
+  <main class="container">
+    <div id="resume" class="flex">
+      <div class="left-col">
+        <h4 class="section-headline">
+          About me
+        </h4>
+        <h4 class="section-headline">
+          Contact
+        </h4>
+        <h4 class="section-headline">
+          Skills
+        </h4>
+        <h4 class="section-headline">
+          Certifications
+        </h4>
+      </div>
+      <div class="right-col">
+        <h4 class="section-headline">
+          Experience
+        </h4>
+        <h4 class="section-headline">
+          Education
+        </h4>
+      </div>
+      <div class="right-col">
+        <div class="personal-name">
+          Your name
+        </div>
+        <div class="personal-title">
+          Job title
+        </div>
+
+        <h4 class="section-headline">
+          Experience
+        </h4>
+        <h4 class="section-headline">
+          Education
+        </h4>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+#resume {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  /* DIN A4 standard paper size. commonly used for resumes
+  For North America letter size use width: 8.5in; height: 11in; */
+  height: 297mm;
+  width: 210mm;
+}
+</style>
